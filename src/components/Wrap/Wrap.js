a5fc43ac719f8e708a0aed3c5dcc6cf1c8ab549a
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-01 17:29:34 +0100

    add regular wrap component

--- src/components/Wrap/Wrap.js
+++ src/components/Wrap/Wrap.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import styled from 'react-emotion';
 
-import config from 'site.config.js';
-import media from 'utils/mediaqueries';
-
-const wrapSizes = config?.responsive?.wrapSizes;
-
-const StyledWrapGrid = styled('div')({
-  margin: '0 auto',
-  width: wrapSizes.mobile,
-  [media('tablet')]: {
-    width: wrapSizes.tablet,
+const StyledWrap = styled('div')(
+  {
+    position: 'relative',
   },
-  [media('tabletFluid')]: {
-    width: wrapSizes.tabletFluid,
-  },
-  [media('laptop')]: {
-    width: wrapSizes.laptop,
-  },
-  [media('desktop')]: {
-    width: wrapSizes.desktop,
-  },
-});
-
-const StyledWrap = styled('div')({
-  margin: '0 auto',
-});
+  props => ({
+    height: props.height,
+  }),
+);
 
 export default function Wrap(props) {
-  const { type, children } = props;
-
-  const Component = type !== 'grid' ? StyledWrapGrid : StyledWrap;
-
-  return <Component>{children}</Component>;
+  const { children, height } = props;
+  return <StyledWrap height={height}>{children}</StyledWrap>;
 }
