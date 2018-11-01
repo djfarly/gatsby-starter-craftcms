1b9ef018eb8a243e1bc6bd8e883d7c4a32269d65
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-01 17:28:47 +0100

    test images

--- src/components/NavigationPrimary/NavigationPrimary.js
+++ src/components/NavigationPrimary/NavigationPrimary.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import styled from 'react-emotion';
 
 import Wrap from 'components/Wrap';
+import WrapGrid from 'components/WrapGrid';
 import Logo from 'components/Logo';
 import Image from 'components/Image';
 import NavigationPrimarySections from 'components/NavigationPrimarySections';
@@ -22,22 +23,24 @@ export default function NavigationPrimary(props) {
   const { navigationTeaser, navigationBackground } = currentItem;
 
   return (
-    <Wrap type="grid">
-      <Logo />
-
-      {allItems.map(item => (
-        <NavigationItem fontSize={16} key={item.navigationEntry[0].id}>
-          {item.navigationEntry[0].title}
-        </NavigationItem>
-      ))}
-
-      <NavigationPrimarySections content={navigationTeaser} />
-
+    <Wrap height="100vh">
       <Image
         src={navigationBackground[0].url}
         alt={navigationBackground[0].title}
-        objectFit="cover"
+        isBackground
+        fit="cover"
       />
+      <WrapGrid>
+        <Logo />
+
+        {allItems.map(item => (
+          <NavigationItem fontSize={16} key={item.navigationEntry[0].id}>
+            {item.navigationEntry[0].title}
+          </NavigationItem>
+        ))}
+
+        <NavigationPrimarySections content={navigationTeaser} />
+      </WrapGrid>
     </Wrap>
   );
 }
