e006ba2172ed59de1ea0ae2ecac64fc1aa8e965a
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-02 19:43:59 +0100

    fix import

--- src/utils/mediaqueries.js
+++ src/utils/mediaqueries.js
@@ -1,4 +1,4 @@
-import config from 'site.config.js';
+import config from '~/site.config.js';
 
 // Setup media query mixins for the following Media features
 // (http://www.w3.org/TR/2012/REC-css3-mediaqueries-20120619/#media1):
