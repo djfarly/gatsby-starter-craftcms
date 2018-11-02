ecd1e092c89a8d463839316774dc62ce0e91baf5
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-02 19:42:18 +0100

    import sorting

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 import { css } from 'emotion';
+
 import themeDefault from 'components/Layout/themeDefault';
 
 export default function Link(props) {
