f8aece58a81f38eefd6df03a2f7d71c3974691aa
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-04 00:31:18 +0100

    fix css injections for link component

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
-import { css } from 'emotion';
+import { cx, css as cssE } from 'emotion';
 
 import themeDefault from 'components/Layout/themeDefault';
 
+const cssHoverDefault = `color: ${themeDefault.colorLinkHover}`;
+
 export default function Link(props) {
-  const { children, display, activecss } = props;
+  const { children, display, cssactive, cssstyles, csshover, to } = props;
+
+  const cssDefault = cssE`
+    color: ${themeDefault.colorLink};
+    text-decoration: none;
+    display: ${display};
+    &:hover {
+      ${csshover ? csshover : cssHoverDefault}
+    }
+    &:active {
+      color: ${themeDefault.colorLinkActive};
+    }
+  `;
+
+  const cssActiveDefault = cssE`
+    color: ${themeDefault.colorLinkCurrent};
+  `;
+
   return (
     <GatsbyLink
-      className={css`
-        color: ${themeDefault.colorLink};
-        text-decoration: none;
-        display: ${display};
-        &:hover {
-          color: ${themeDefault.colorLinkHover};
-        }
-        &:active {
-          color: ${themeDefault.colorLinkActive};
-        }
-      `}
-      activeClassName={
-        activecss
-          ? css`
-              ${activecss};
-            `
-          : css`
-              color: ${themeDefault.colorLinkCurrent};
-            `
-      }
-      {...props}
+      className={cx(
+        cssE`
+          ${cssstyles};
+        `,
+        cssDefault,
+      )}
+      activeClassName={cx(
+        cssE`
+          ${cssactive};
+        `,
+        cssActiveDefault,
+      )}
+      to={to}
     >
       {children}
     </GatsbyLink>
