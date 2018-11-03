7bdd154234fed7018d7e7d5aa25f2047f269e436
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-04 00:31:40 +0100

    polyfill logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'react-emotion';
 
-const StyledLogo = styled('div')({}, props => ({
+import Link from 'components/Link';
+
+const StyledLogo = styled('span')({}, props => ({
   color: props.theme.colorPrimary,
+  fontWeight: 900,
+  letterSpacing: '3px',
+  textTransform: 'uppercase',
 }));
 
 export default function Logo() {
-  // const { src, alt, objectFit } = props;
-  return <StyledLogo>Evo Fitness</StyledLogo>;
+  return (
+    <Link csshover="color: #00fff8;" to="/de/start">
+      <StyledLogo>Evo Fitness</StyledLogo>
+    </Link>
+  );
 }
