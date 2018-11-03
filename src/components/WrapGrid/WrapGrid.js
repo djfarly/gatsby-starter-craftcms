6dd22c1a2daeb7f3f2ccfd9dc5969bf2b5000f87
David Hoffmann <david.hoffmann@dunckelfeld.de>
2018-11-04 00:33:20 +0100

    fix grid wrapping and breakpoints

--- src/components/WrapGrid/WrapGrid.js
+++ src/components/WrapGrid/WrapGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
 import config from '~/site.config.js';
@@ -6,25 +7,43 @@ import media from 'utils/mediaqueries';
 
 const wrapSizes = config?.responsive?.wrapSizes;
 
-const StyledWrapGrid = styled('div')({
-  position: 'relative',
-  margin: '0 auto',
-  width: wrapSizes.mobile,
-  [media('tablet')]: {
-    width: wrapSizes.tablet,
+const StyledWrapGrid = styled('div')(
+  {
+    position: 'relative',
+    width: wrapSizes.mobile,
+    [media('tablet')]: {
+      width: wrapSizes.tablet,
+    },
+    [media('tabletFluid')]: {
+      width: wrapSizes.tabletFluid,
+    },
+    [media('laptop')]: {
+      width: wrapSizes.laptop,
+    },
+    [media('desktop')]: {
+      width: wrapSizes.desktop,
+    },
   },
-  [media('tabletFluid')]: {
-    width: wrapSizes.tabletFluid,
-  },
-  [media('laptop')]: {
-    width: wrapSizes.laptop,
-  },
-  [media('desktop')]: {
-    width: wrapSizes.desktop,
-  },
-});
+  props => ({
+    margin: props.margin,
+    padding: props.padding,
+    height: props.height,
+  }),
+);
 
 export default function WrapGrid(props) {
-  const { children } = props;
-  return <StyledWrapGrid>{children}</StyledWrapGrid>;
+  const { children, margin, padding, height } = props;
+  return (
+    <StyledWrapGrid margin={margin} padding={padding} height={height}>
+      {children}
+    </StyledWrapGrid>
+  );
 }
+
+WrapGrid.propTypes = {
+  margin: PropTypes.string,
+};
+
+WrapGrid.defaultProps = {
+  margin: '0 auto',
+};
